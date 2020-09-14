c841627ed681fdd77d8dcaf903a23ef342b19e9e
MaryamGhiasvand <m.ghiyasvand001@gmail.com>
2020-09-13 23:41:35 -0500

    Add AntMatchers to create URL whiteList

--- demo/src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ demo/src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -4,6 +4,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -12,6 +16,8 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                .antMatchers("/","index")
+                .permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
