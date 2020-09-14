b39c6367a50170f2593e3bebe5aca485368a34ac
MaryamGhiasvand <m.ghiyasvand001@gmail.com>
2020-09-14 00:16:27 -0500

    Add passwordEncoder By Bcrypt

--- demo/src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ demo/src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -1,5 +1,7 @@
 package com.example.demo.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -7,11 +9,20 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+    //wire this class with passwordEncoder to use it for password
+    private final PasswordEncoder passwordEncoder;
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -25,13 +36,19 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    //@Bean ? why
+    @Bean //? why
     protected UserDetailsService userDetailsService() {
         UserDetails newUser = User.builder()
                 .username("Maryam")
-                .password("123")
+                .password(passwordEncoder.encode("123")) //to encode the password to bcrypt
                 .roles("STUDENT") //will be matched to ROLE_STUDENT
                 .build();
-        return new InMemoryUserDetailsManager(newUser);
+
+        UserDetails adminUser = User.builder()
+                .username("Admin")
+                .password(passwordEncoder.encode("123")) //to encode the password to bcrypt
+                .roles("ADMIN") //will be matched to ROLE_STUDENT
+                .build();
+        return new InMemoryUserDetailsManager(newUser,adminUser);
     }
 }
