8d85adab870a57cae7311a08c436beb05f4a5414
MaryamGhiasvand <m.ghiyasvand001@gmail.com>
2020-09-18 12:19:59 -0500

    Get the token from the header of client request

--- demo/src/main/java/com/example/demo/jwt/JwtTokenVerifier.java
+++ demo/src/main/java/com/example/demo/jwt/JwtTokenVerifier.java
@@ -1,5 +1,6 @@
 package com.example.demo.jwt;
 
+import com.google.common.base.Strings;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -12,8 +13,16 @@ import java.io.IOException;
 public class JwtTokenVerifier extends OncePerRequestFilter {//this filter should be executed exactly once per req
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        //1)Get token from request:
-        String authorizarionHeader= request.getHeader("Authorization"); //the tokenName
+        //1)Get token from request: request.getHeader("tokenName") = Bearer +realToken
+        String token= request.getHeader("authorization"); //authorization=the tokenName
+
+        //from com.google.common.base.Strings library
+        if(Strings.isNullOrEmpty(token) || !token.startsWith("Bearer ")){
+            //some thing is wrong => reject the request
+            filterChain.doFilter(request,response); //dofilter = reject
+        }
+
+        String exactToken = token.replace("Bearer ",""); //remove "Bearer " from beginning of token
     }
 
 }
